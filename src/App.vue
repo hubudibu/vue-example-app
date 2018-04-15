a8d3687debe413f751fe56f57830593647fbc25a
Aniko Fejes <oranjje@gmail.com>
2018-04-15 19:20:52 +0200

    feat(comments): commentedarticle instead of helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <CommentedArticle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import CommentedArticle from './components/CommentedArticle';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    CommentedArticle,
   },
 };
 </script>
@@ -21,7 +21,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/CommentedArticle.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>Animals Riding Animals</h1>
+    <p>James Corden egy új szegmenssel jött, amiben a közönségének tagjai csodálatos nyereményekért tippelheti meg, hogy a képen látott állat milyen másik állaton lovagol. A képek is aranyosak, de a felvezetésben is akad pár poén a tovább mögött.</p>
+    <h2>Comments</h2>
+    <div>
+      <input type="text" placeholder="name">
+      <textarea placeholder="comment" cols="30" rows="10"></textarea>
+      <button>Submit</button>
+    </div>
+    <ul>
+      <li>
+        <h3>Michael J Szymanski</h3>
+        <p>Interesting article</p>
+      </li>
+      <li>
+        <h3>Cory J Harris</h3>
+        <p>Yes</p>
+      </li>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+input, textarea {
+  width: 100%;
+  font-size: 16px;
+  padding: .5em;
+}
+</style>
